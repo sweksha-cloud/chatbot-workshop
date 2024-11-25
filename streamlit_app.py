01d257e7d42fd73d97863e6f9e64af0614394da3
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-11-25 11:15:39 -0800

    Remove typo from title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from llama_index.embeddings.gemini import GeminiEmbedding
 
 
 st.set_page_config(page_title="Chat with an expert on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with an export on the works of Rabindranath Tagore")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.title("Chat with an expert on the works of Rabindranath Tagore")
+
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
