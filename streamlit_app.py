5422b43f98e957bbab8c221c98149f9719e16d2b
sweksha-cloud <swekshas123@gmail.com>
2025-09-11 13:18:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,9 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-2.5-flash",
         temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
+        system_prompt="""You are an expert on the works of Sweksha Shaw.
+        Answer the question using the provided documents, which contain relevant information about of Sweksha Shaw.
+        The context for all questions is the work of Sweksha Shaw. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
         Respond using a florid but direct tone, typical of an early modernist writer.
         Keep your answers under 100 words.""",
         api_key = st.secrets.google_gemini_key,
