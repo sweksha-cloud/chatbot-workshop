61455341ac84d7b675fdd187e0466d8e24853b6c
sweksha-cloud <swekshas123@gmail.com>
2025-09-11 13:03:47 -0700

    Update chatbot title to my name

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ import nltk
 def get_stopwords():
     nltk.download('stopwords')
 
-st.set_page_config(page_title="Chat with a friend on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with a friend on the works of Rabindranath Tagore")
+st.set_page_config(page_title="Chat with a friend on the works of Sweksha Shaw", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with a friend on the works of Sweksha Shaw")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
